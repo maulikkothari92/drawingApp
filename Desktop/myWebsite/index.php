c617d081fefe2a6717f421bb386fa6337612dd6d
maulikkothari92 <maulikkothari92@gmail.com>
2014-04-15 14:03:43 -0400

    Adding pages

---
+++ Desktop/myWebsite/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title> My application </title>
+</head>
+<body>
+    <p>Welcome to my website</p>
+</body>
+</html>
+    
